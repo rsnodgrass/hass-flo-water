2dd3db14b78f1b228ac66248b1ba3a5ecaef1da4
btuysserkani <btuysserkani@nvidia.com>
2020-02-03 09:09:09 -0700

    Add FloConsumptionSensor.

--- custom_components/flo/sensor.py
+++ custom_components/flo/sensor.py
@@ -7,11 +7,14 @@ FUTURE:
 - should this use Flo's every 15-minutes average rollup instead of current telemetry?
 - could change to non-polling mode (since the "switch" does the actual polling, these would just update whenever the switch detects a state change)
 """
+from datetime import datetime, timedelta
 import logging
 import voluptuous as vol
 
 from homeassistant.const import TEMP_FAHRENHEIT, ATTR_TEMPERATURE
+from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.util import dt as dt_util
 import homeassistant.helpers.config_validation as cv
 
 from pyflowater.const import FLO_V2_API_PREFIX, FLO_MODES, FLO_AWAY, FLO_HOME, FLO_SLEEP
@@ -22,6 +25,8 @@ LOG = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_LOCATION_ID): cv.string
 })
+
+TIME_FMT = '%Y-%m-%dT%H:%M:%S.000Z'
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_sensors_callback, discovery_info=None):
@@ -44,9 +49,10 @@ def setup_platform(hass, config, add_sensors_callback, discovery_info=None):
 
     # iterate all devices and create a valve switch for each device
     sensors = []
+    sensors.append(FloConsumptionSensor(hass, flo, location_id))
     for device in location['devices']:
         device_id = device['id']
-        
+
         sensors.append( FloRateSensor(hass, device_id) )
         sensors.append( FloTempSensor(hass, device_id) )
         sensors.append( FloPressureSensor(hass, device_id) )
@@ -141,9 +147,89 @@ class FloPressureSensor(FloEntity):
     def update(self):
         """Update sensor state"""
         state = self.get_telemetry('psi')
-        if self._state != state:
+        if self._state != round(state, 2):
             self._state = round(state, 2)
             LOG.info("Updated %s to %f %s", self._name, self._state, self.unit_of_measurement)
+
+class FloConsumptionSensor(Entity):
+    """Water consumption sensor for a Flo device location"""
+
+    def __init__(self, hass, flo, location_id):
+        # super().__init__(hass, device_id)
+        self._name = "Flo Water Consumption"
+        self._state = None
+        self._last_end = 0
+        self._flo = flo
+        self._location_id = location_id
+        self._attrs = {"location_id": location_id}
+        self.initial_update()
+
+    @property
+    def name(self):
+        """Return the display name for this sensor"""
+        return self._name
+
+    @property
+    def device_state_attributes(self):
+        """Return the device state attributes."""
+        return self._attrs
+
+    @property
+    def unit_of_measurement(self):
+        """gallons (g)"""
+        return "gallons"
+
+    @property
+    def state(self):
+        """Gallons"""
+        return self._state
+
+    @property
+    def icon(self):
+        return "mdi:gauge"
+
+    def readConsumption(self, start, end, interval):
+        res = self._flo.consumption(self._location_id, start.strftime(TIME_FMT), end.strftime(TIME_FMT), interval)
+        return round(res['aggregations']['sumTotalGallonsConsumed'], 2)
+
+    def initial_update(self):
+        """ Initial update sensor state"""
+        # get consumption for the whole year
+        end = dt_util.utcnow()
+        start = end - timedelta(days=365)
+        self._state = self.readConsumption(start,end,'1m')
+        self._total = self._state
+        LOG.info(
+                "Updated %s to %f %s", self._name, self._state, self.unit_of_measurement
+            )
+        self._last_end = end
+
+    def update(self):
+        """Update sensor state"""
+        now = dt_util.utcnow()
+        if now.hour != self._last_end.hour:
+            # if we crossed over an hour boundary, add the last hour to the total
+            end = now.replace(minute=0, second=0, microsecond=0)
+            start = end - timedelta(hours=1)
+            prev_hour = self.readConsumption(start,end,'1h')
+            self._total += prev_hour
+
+        # flo counts all previous consumption in the first second of the hour.
+        # don't double count if we are in the 1st second and just crossed over
+        if now.hour != self._last_end.hour and now.second == 0:
+            curr = 0
+        else:
+            start = now - timedelta(hours=1)
+            curr = self.readConsumption(start, now, '1h')
+
+        self.readConsumption(now-timedelta(days=365),now,'1m')
+        state = self._total + curr
+        self._last_end = now
+        if self._state != state:
+            self._state = state
+            LOG.info(
+                "Updated %s to %f %s", self._name, self._state, self.unit_of_measurement
+            )
 
 # https://support.meetflo.com/hc/en-us/articles/115003927993-What-s-the-difference-between-Home-Away-and-Sleep-modes-
 class FloMonitoringMode(FloEntity):
